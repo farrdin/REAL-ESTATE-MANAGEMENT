b9362673d5af03a2516207ee60806eb61c890178
Fardin Ahmed Alif <Alif.Ahmed44@gmail.com>
2024-04-28 04:54:50 +0600

    updated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,6 +3,12 @@ const Home = () => {
     <div>
       <h1>Hi This is New Page</h1>
       <h2>Hell0</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem,
+        blanditiis atque laboriosam exercitationem dolore aut quam possimus sed?
+        Temporibus unde nobis quo velit, accusamus doloribus ipsa minima nam
+        totam similique.
+      </p>
     </div>
   );
 };
