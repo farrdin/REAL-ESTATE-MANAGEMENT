fdfe0d107b295a6da7084f3b1af790a099b411ab
Fardin Ahmed Alif <Alif.Ahmed44@gmail.com>
2024-05-05 02:31:37 +0600

    new route design

--- src/Pages/DetailProperty.jsx
+++ src/Pages/DetailProperty.jsx
@@ -1,5 +1,5 @@
 import { Helmet } from "react-helmet-async";
-import { useLoaderData, useParams } from "react-router-dom";
+import { Link, useLoaderData, useParams } from "react-router-dom";
 import { buyLater } from "../Utilities/LocalStorage";
 
 const DetailProperty = () => {
@@ -26,18 +26,69 @@ const DetailProperty = () => {
       <Helmet>
         <title>EstateVista | Details-{id} Details</title>
       </Helmet>
-      <img src={image} alt="" />
-      {estate_title}
-      {area}
-      {description}
-      {facilities}
-      {location}
-      {price}
-      {segment_name}
-      {status}
-      <button onClick={buyLaterWish} className="btn btn-secondary">
-        Buy Later
-      </button>
+
+      <div className="flex flex-col lg:flex-row gap-5 border rounded-2xl p-5">
+        <div className="w-1/2 flex flex-col space-y-5 items-center">
+          <img className="rounded-xl" src={image} alt="" />
+          <button onClick={buyLaterWish} className="btn btn-secondary w-2/3">
+            Add to wishlist
+          </button>
+          <button className="btn btn-primary w-2/3">Book Now</button>
+        </div>
+        <div className="w-1/2">
+          <div className="space-y-3">
+            <h1 className="text-xl font-bold font-work text-[#282828]">
+              {estate_title}
+            </h1>
+            <p className="text-[#282828B2] text-sm">
+              <span className="text-base font-medium font-work text-[#282828]">
+                Space :
+              </span>
+              {area}
+            </p>
+            <p className="text-base font-work text-[#282828B2] text-poppins">
+              <span className="text-base font-medium font-work text-[#282828]">
+                Description :
+              </span>
+              {description}
+            </p>
+            <p className="text-base font-work font-medium text-[#282828B2]">
+              <span className="text-base font-medium font-work text-[#282828]">
+                Facilities :
+              </span>
+              <ul className="list-disc pl-5">
+                <li>{facilities[0]}</li>
+                <li>{facilities[1]}</li>
+                <li>{facilities[2]}</li>
+              </ul>
+            </p>
+            <p className="text-base font-work text-[#282828B2] text-poppins">
+              <span className="text-base font-medium font-work text-[#282828]">
+                Location :
+              </span>
+              {location}
+            </p>
+            <p className="text-base font-work text-[#282828B2] text-poppins">
+              <span className="text-base font-medium font-work text-[#282828]">
+                Price :
+              </span>
+              {price}
+            </p>
+            <p className="text-base font-work text-[#282828B2] text-poppins">
+              <span className="text-base font-medium font-work text-[#282828]">
+                Segment :
+              </span>
+              {segment_name}
+            </p>
+            <p className="text-base font-work text-[#282828B2] text-poppins">
+              <span className="text-base font-medium font-work text-[#282828]">
+                Status :
+              </span>
+              {status}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Pages/WishList.jsx
+++ src/Pages/WishList.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Helmet } from "react-helmet-async";
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import { getBuyLaterItem } from "../Utilities/LocalStorage";
 
 const WishList = () => {
@@ -21,15 +21,59 @@ const WishList = () => {
       <Helmet>
         <title>EstateVista | Wishlist</title>
       </Helmet>
-      <div className="grid grid-cols-3 gap-5 my-5">
+      <div className="p-5 space-y-5">
         {wishlist &&
           wishlist.map((list) => (
-            <div key={list.id}>
-              <img
-                src={list.image}
-                className="rounded-lg h-[240px] w-full"
-                alt=""
-              />
+            <div
+              key={list.id}
+              className="border rounded-2xl flex flex-col  lg:flex lg:flex-row justify-around items-center p-3"
+            >
+              <div className="h-40">
+                <img src={list.image} className="rounded-lg h-full w-full" />
+              </div>
+              <div>
+                <div className="space-y-3">
+                  <h1 className="text-xl font-bold font-work text-[#282828]">
+                    {list.estate_title}
+                  </h1>
+                  <p className="text-[#282828B2] text-sm">
+                    <span className="text-base font-medium font-work text-[#282828]">
+                      Space :
+                    </span>
+                    {list.area}
+                  </p>
+
+                  <p className="text-base font-work text-[#282828B2] text-poppins">
+                    <span className="text-base font-medium font-work text-[#282828]">
+                      Location :
+                    </span>
+                    {list.location}
+                  </p>
+                  <p className="text-base font-work text-[#282828B2] text-poppins">
+                    <span className="text-base font-medium font-work text-[#282828]">
+                      Price :
+                    </span>
+                    {list.price}
+                  </p>
+                  <p className="text-base font-work text-[#282828B2] text-poppins">
+                    <span className="text-base font-medium font-work text-[#282828]">
+                      Segment :
+                    </span>
+                    {list.segment_name}
+                  </p>
+                  <p className="text-base font-work text-[#282828B2] text-poppins">
+                    <span className="text-base font-medium font-work text-[#282828]">
+                      Status :
+                    </span>
+                    {list.status}
+                  </p>
+                </div>
+              </div>
+              <div>
+                <Link to={`/details/${list.id}`}>
+                  <button className="btn btn-secondary">Buy Now</button>
+                </Link>
+              </div>
             </div>
           ))}
       </div>
